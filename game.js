02b3b7554a443386749d756d4b55ae2fee969ad7
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-05-06 21:47:44 +0200

    handSelection

--- game.js
+++ game.js
@@ -6,3 +6,21 @@ const gameSummary = {
     losses: 0,
     draws: 0
 }
+
+const game = {
+    playerHand: '',
+    aiHand: '',
+    playerHandHTML: ''
+}
+
+const hands = document.querySelectorAll('.select img')
+
+function handSelection() {
+    game.playerHand = this.dataset.option;
+    console.log(game.playerHand);
+    hands.forEach(hand => hand.style.boxShadow = '');
+    this.style.boxShadow = '0 0 5px 3px pink'
+}
+
+
+hands.forEach(hand => hand.addEventListener('click', handSelection))
